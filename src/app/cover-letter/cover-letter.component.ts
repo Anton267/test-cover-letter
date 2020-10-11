dd7dea3125b5e45636551110f6d0aa8e178d0a50
Anton14889 <jalssomino@gmail.com>
2020-10-11 16:57:40 +0300

    add FormGroup

--- src/app/cover-letter/cover-letter.component.ts
+++ src/app/cover-letter/cover-letter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CoverLetterService } from '../shared/services/cover-letter.service';
 
 @Component({
   selector: 'app-cover-letter',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoverLetterComponent implements OnInit {
 
-  constructor() { }
+  letterForm: FormGroup;
+
+  constructor(
+    public coverLetterService: CoverLetterService,
+    private fb: FormBuilder,
+  ) {
+    this.letterForm = this.fb.group({
+      // id: [''],
+      profession: ['', Validators.minLength(3), Validators.required],
+      name: ['', Validators.minLength(3), Validators.required],
+      about: ['',  Validators.minLength(3), Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
