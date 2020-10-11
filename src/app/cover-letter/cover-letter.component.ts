504f7c08d73f9c91967389142a654b9f42a4187e
Anton14889 <jalssomino@gmail.com>
2020-10-11 19:53:58 +0300

    add letter

--- src/app/cover-letter/cover-letter.component.ts
+++ src/app/cover-letter/cover-letter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Letter } from '../shared/interfaces/letter.interface';
 import { CoverLetterService } from '../shared/services/cover-letter.service';
 
 @Component({
@@ -29,16 +30,34 @@ export class CoverLetterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public saveLetter(): void {
+    const letter: Letter = {
+      id: this.id.value,
+      profession: this.profession.value,
+      name: this.name.value,
+      about: this.about.value,
+    };
+    this.coverLetterService.addLetter(letter);
+  }
+
   private setrandomID(): string {
     return Math.random().toString(36).substring(2, 15);
   }
 
-  public get id(): AbstractControl { return this.letterForm.get('id'); }
+  public get id(): AbstractControl {
+    return this.letterForm.get('id');
+  }
 
-  public get profession(): AbstractControl { return this.letterForm.get('profession'); }
+  public get profession(): AbstractControl {
+    return this.letterForm.get('profession');
+  }
 
-  public get name(): AbstractControl { return this.letterForm.get('name'); }
+  public get name(): AbstractControl {
+    return this.letterForm.get('name');
+  }
 
-  public get about(): AbstractControl { return this.letterForm.get('about'); }
+  public get about(): AbstractControl {
+    return this.letterForm.get('about');
+  }
 
 }
