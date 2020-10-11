b101cec2c481614c6ffa07791cf2b17354dc1eaf
Anton14889 <jalssomino@gmail.com>
2020-10-11 17:12:12 +0300

    add get form controls

--- src/app/cover-letter/cover-letter.component.ts
+++ src/app/cover-letter/cover-letter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CoverLetterService } from '../shared/services/cover-letter.service';
 
 @Component({
@@ -16,14 +16,26 @@ export class CoverLetterComponent implements OnInit {
     private fb: FormBuilder,
   ) {
     this.letterForm = this.fb.group({
-      // id: [''],
+      id: [this.setrandomID()],
       profession: ['', [Validators.minLength(3), Validators.required]],
       name: ['', [Validators.minLength(3), Validators.required]],
-      about: ['',  [Validators.minLength(3), Validators.required]],
+      about: ['', [Validators.minLength(3), Validators.required]],
     });
   }
 
   ngOnInit(): void {
   }
+
+  private setrandomID(): string {
+    return Math.random().toString(36).substring(2, 15);
+  }
+
+  public get id(): AbstractControl { return this.letterForm.get('id'); }
+
+  public get profession(): AbstractControl { return this.letterForm.get('profession'); }
+
+  public get name(): AbstractControl { return this.letterForm.get('name'); }
+
+  public get about(): AbstractControl { return this.letterForm.get('about'); }
 
 }
