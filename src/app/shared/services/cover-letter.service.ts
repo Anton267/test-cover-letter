02892988f91d5cdf61c658054861741f591ff478
Anton14889 <jalssomino@gmail.com>
2020-10-11 16:48:01 +0300

    add letters methods

--- src/app/shared/services/cover-letter.service.ts
+++ src/app/shared/services/cover-letter.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Letter } from '../interfaces/letter.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoverLetterService {
 
+  public letters: Letter[] = [];
+
   constructor() { }
+
+  public addLetter(letter: Letter): void {
+    this.letters.push(letter);
+  }
+
+  public removeLetter(letter: Letter): void {
+    const index = this.letters.findIndex((el: Letter) => el.id ===  letter.id);
+    this.letters.splice(index, 1);
+  }
+
+  public editLetter(letter: Letter): void {
+    const index = this.letters.findIndex((el: Letter) => el.id ===  letter.id);
+    this.letters.splice(index, 1, letter);
+  }
 }
