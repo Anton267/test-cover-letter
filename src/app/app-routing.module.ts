dc1912f946029b39e9dc0decc6527f05b9c1ad1a
Anton14889 <jalssomino@gmail.com>
2020-10-11 16:23:46 +0300

    add routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoverLetterComponent } from './cover-letter/cover-letter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CoverLetterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
